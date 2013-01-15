94bda0aa627388ba179e751820ee04cf9d9c98db
Moritz Kiefer <moritz.kiefer@gmail.com>
2013-01-15 20:13:54 +0100

    another try

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='diaspy',
+      version='1.0',
+      author='Moritz Kiefer',
+      author_email='moritz.kiefer@gmail.com',
+      py_modules=['diaspy.diaspy'],
+      requires=['requests']
+      )
