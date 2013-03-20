f049c3977a8f4f5aff99e6a33d5fedb6b85713da
Marek Marecki <triviuss@gmail.com>
2013-03-20 22:44:18 +0100

    Client() in diaspy/__init__.py documented

--- diaspy/__init__.py
+++ diaspy/__init__.py
@@ -3,6 +3,16 @@ import diaspy.models
 import diaspy.conversations
 
 class Client(diaspy.client.Client):
-    """Wrapper class for easier imports.
+    """Class created for easier imports of client. 
+    Instead of:
+
+        import diaspy
+        client = diaspy.client.Client(...)
+
+    you can simply use:
+
+        import diaspy
+        client = diaspy.Client(...)
+
     """
     pass
