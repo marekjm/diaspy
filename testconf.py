68d7af9b1117fdcb9fcc239c73010023c72f0756
Marek Marecki <triviuss@gmail.com>
2013-03-29 12:30:09 +0100

    get_*() methods refactored to use _sessionget()

--- testconf.py
+++ testconf.py
@@ -12,6 +12,6 @@
 #   Their values have to be valid.
 
 #   __pod__ = 'https://pod.example.com'
-__pod__ = 'https://pod.orkz.net'
-__username__ = 'marekjm'
-__passwd__ = 'mAreKJmonDiASporA'
+__pod__ = ''
+__username__ = ''
+__passwd__ = ''
