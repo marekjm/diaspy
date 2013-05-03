c8e8dd74bc8801b6c97932a81180960319714046
Moritz Kiefer <moritz.kiefer@gmail.com>
2013-05-03 10:19:28 +0200

    Increment version to 0.0.2

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 setup(name='diaspy',
-      version='0.0.1',
+      version='0.0.2',
       author='Moritz Kiefer',
       author_email='moritz.kiefer@gmail.com',
+      url='https://github.com/Javafant/diaspora-api',
+      description='A python api to the social network diaspora',
       packages=find_packages(),
       install_requires=['requests']
       )
