a91996ead618492a8a90ffe68c86959ab8b829ed
Marek Marecki <triviuss@gmail.com>
2013-07-06 22:13:53 +0200

    Moved `lookup_user()` method from `Connection()` to `Search()`

--- diaspy/search.py
+++ diaspy/search.py
@@ -7,5 +7,14 @@
 class Search():
     """This object is used for searching for content on Diaspora*.
     """
-    def __init__(self):
-        pass
+    def __init__(self, connection):
+        self._connection = connection
+
+    def lookup_user(self, handle):
+        """This function will launch a webfinger lookup from the pod for the
+        handle requested. Nothing is returned but if the lookup was successful,
+        user should soon be searchable via this pod.
+        
+        :param string: Handle to search for.
+        """
+        request = self.get('people', headers={'accept': 'text/html'}, params={'q':handle})
