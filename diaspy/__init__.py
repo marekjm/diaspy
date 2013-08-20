aacbca873bd40df615fb329590bb39be719891b4
Marek Marecki <marekjm@taistelu.com>
2013-08-20 13:19:52 +0200

    Fixed missing connection import in __init__

--- diaspy/__init__.py
+++ diaspy/__init__.py
@@ -1,9 +1,10 @@
+import diaspy.connection as connection
 import diaspy.models as models
-import diaspy.conversations as conversations
 import diaspy.streams as streams
+import diaspy.conversations as conversations
 import diaspy.people as people
 import diaspy.notifications as notifications
 import diaspy.settings as settings
 
 
-__version__ = '0.3.2'
+__version__ = '0.3.2.1'
