bb618f97c2a0311c4dc29f0a44c717fb7c8c1ef5
Marek Marecki <marekjm@taistelu.com>
2014-01-11 23:57:33 +0100

    Updated setup.py file

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 setup(name='diaspy',
       version='0.4.2',
-      author='Moritz Kiefer',
-      author_email='moritz.kiefer@gmail.com',
-      url='https://github.com/Javafant/diaspora-api',
-      description='A Python API to the social network Diaspora*',
+      author='Marek Marecki',
+      author_email='hi@ozro.pw',
+      url='https://github.com/marekjm/diaspy',
+      description='Unofficial Python API to the social network Diaspora*',
       packages=find_packages(),
       install_requires=['requests']
       )
